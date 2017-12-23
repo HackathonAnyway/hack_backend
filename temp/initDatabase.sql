b8165b6239adf3cabad05ed4a8eff3dc4141b5e6
william huang <hylbegoat@gmail.com>
2017-12-23 15:55:05 +0800

    enhance init.sql

--- temp/initDatabase.sql
+++ temp/initDatabase.sql
@@ -1,12 +1,14 @@
+grant all privileges on *.* to root@'%' identified by "123456";
+flush privileges;
 CREATE DATABASE hackforchristmas;
 
 USE hackforchristmas;
 
 CREATE TABLE TB_EVENT (
-    eventId varchar(50),
-    eventName varchar(100),
+    eventId varchar(50) NOT NULL UNIQUE PRIMARY KEY,
+    eventName varchar(100) NOT NULL,
     eventStarttime varchar(15),
-    enventLocation varchar(200),
-    eventPeriod varchar(10),
-    evetnFlag int
+    enventLocation varchar(200) NOT NULL,
+    eventPeriod varchar(10) NOT NULL,
+    evetnFlag int NOT NULL
 );
