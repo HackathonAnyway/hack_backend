d87b45435b757aa6975551b9dff16f2efaadadb4
william huang <hylbegoat@gmail.com>
2017-12-23 21:52:26 +0800

    unstable need unittest

--- hack.py
+++ hack.py
@@ -14,31 +14,43 @@ def happy_birthday(name, age:hug.types.number=1):
 VERSION = 1
 @hug.post('/event/add', versions=VERSION)
 def addEvent(eventName, location, starttime, period, flag:hug.types.number=0):
-    cnx = connectDb.getConn()
-    with cnx.cursor() as cursor:
-        sql = 'INSERT INTO TB_EVENT VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\')'.format(str(uuid.uuid4()), eventName, starttime, location, period, flag)
-        print (sql)
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        cnx.commit()
-    cnx.close()
-    return
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            sql = 'INSERT INTO TB_EVENT VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\')'.format(str(uuid.uuid4()), eventName, starttime, location, period, flag)
+            cursor.execute(sql)
+            resResult = "success"
+            cnx.commit()
+    except Exception as err:
+        resResult = "error occurred"
+        print (err)
+    finally:
+        cnx.close()
+        return resResult
+    
 
 @hug.post('/event/delete', versions=VERSION)
 def deleteEvent(userId, eventId, response=None):
-    cnx = connectDb.getConn()
-    with cnx.cursor() as cursor:
-        sqlSelect = 'SELECT * FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
-        cursor.execute(sqlSelect)
-        result = cursor.fetchall()
-        if result:
-            sqlDelete = 'DELETE FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
-            cursor.execute(sqlDelete)
-            cnx.commit()
-        else :
-            response.status = HTTP_400
-            return "event not found"
-    cnx.close()
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            sqlSelect = 'SELECT * FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
+            cursor.execute(sqlSelect)
+            resResult = cursor.fetchall()
+            if resResult:
+                sqlDelete = 'DELETE FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
+                cursor.execute(sqlDelete)
+                result = "delete success"
+                cnx.commit()
+            else :
+                response.status = HTTP_400
+                result = "event not found"
+    except Exception as err:
+        result = "error occurred"
+        print (err)
+    finally:
+        cnx.close()
+        return result
 
 @hug.get('/event/query', versions=VERSION)
 def queryEvent(userId, eventId="DEFAULT"):
@@ -61,30 +73,76 @@ def queryEvent(userId, eventId="DEFAULT"):
         ]
     }
     """
-    cnx = connectDb.getConn()
-    with cnx.cursor() as cursor:
-        if eventId == "DEFAULT":
-            sql = 'SELECT * FROM TB_EVENT'
-        else :
-            sql = 'SELECT * FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        cnx.commit()
-    cnx.close()
-    print(result)
-    return result
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            if eventId == "DEFAULT":
+                sql = 'SELECT * FROM TB_EVENT'
+            else :
+                sql = 'SELECT * FROM TB_EVENT WHERE eventId=\'{0}\''.format(eventId)
+            cursor.execute(sql)
+            resResult = cursor.fetchall()
+            cnx.commit()
+    except Exception as err:
+        resResult = "error occurred"
+        print (err)
+    finally:
+        cnx.close()
+        return resResult
 
 @hug.post('/event/modify', versions=VERSION)
 def modifyEvent(eventId, eventName, eventStarttime, eventLocation, eventPeriod, eventFlag):
-    cnx = connectDb.getConn()
-    with cnx.cursor() as cursor:
-        sql = 'UPDATE TB_EVENT SET eventName=\'{0}\', eventStarttime=\'{1}\', eventLocation=\'{2}\',\
-             eventPeriod=\'{3}\', eventFlag=\'{4}\' WHERE eventId=\'{5}\'\
-            '.format(eventName, eventStarttime, eventLocation, eventPeriod, eventFlag, eventId)
-        cursor.execute(sql)
-        cnx.commit()
-    cnx.close
-    print(reversed)
-    return
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            sql = 'UPDATE TB_EVENT SET eventName=\'{0}\', eventStarttime=\'{1}\', eventLocation=\'{2}\',\
+                eventPeriod=\'{3}\', eventFlag=\'{4}\' WHERE eventId=\'{5}\'\
+                '.format(eventName, eventStarttime, eventLocation, eventPeriod, eventFlag, eventId)
+            cursor.execute(sql)
+            resResult = cursor.fetchall()
+            cnx.commit()           
+    except Exception as err:
+        resResult = "error occurred"
+        print (err)
+    finally:
+        cnx.close()
+        return resResult
 
-
+@hug.post('/user/login', versions=VERSION)
+def loginUser(userId, userPassword, response=None):
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            sql = 'SELECT userPassword FROM TB_USER WHERE userId ={0}'.format(userId)
+            print (sql)
+            cursor.execute(sql)
+            resResult = cursor.fetchall()
+            if (resResult[0]['userPassword'] == userPassword):
+                result = 'success'
+            else:
+                response.status = HTTP_400
+                result = 'auth error'
+            cnx.commit()    
+    except Exception as err:
+        result = "auth error"
+        response.status = HTTP_400
+        print (err)
+    finally:
+        cnx.close()
+        return result
+        
+@hug.post('/user/register', versions=VERSION)
+def registerUser(userId, userPassword):
+    try:
+        cnx = connectDb.getConn()
+        with cnx.cursor() as cursor:
+            sql = 'INSERT INTO TB_USER VALUES({0}, {1})'.format(userId, userPassword)
+            cursor.execute(sql)
+            resResult = "success"
+            cnx.commit()
+    except Exception as err:
+        resResult = "error occurred"
+        print (err)
+    finally:
+        cnx.close()
+        return resResult
