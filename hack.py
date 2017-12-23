6a2206a29c1e118b85ada13281689ba3a5e0ae50
william huang <hylbegoat@gmail.com>
2017-12-23 14:55:57 +0800

    basic add

--- hack.py
+++ hack.py
@@ -2,6 +2,7 @@
 """A basic (single function) API written using hug"""
 import hug
 import connectDb
+import uuid
 
 @hug.post('/happy_birthday')
 def happy_birthday(name, age:hug.types.number=1):
@@ -10,8 +11,16 @@ def happy_birthday(name, age:hug.types.number=1):
 
 VERSION = 1
 @hug.post('/event/add', versions=VERSION)
-def addEvent(eventName, location, starttime, period):
-    pass
+def addEvent(eventName, location, starttime, period, flag:hug.types.number=0):
+    cnx = connectDb.getConn()
+    with cnx.cursor() as cursor:
+        sql = 'INSERT INTO TB_EVENT VALUES(\'{0}\', \'{1}\', \'{2}\', \'{3}\', \'{4}\', \'{5}\')'.format(str(uuid.uuid4()), eventName, starttime, location, period, flag)
+        print (sql)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print (result)
+        cnx.commit()
+    cnx.close()
 
 @hug.post('/event/delete', versions=VERSION)
 def deleteEvent(userId, eventId):
@@ -21,7 +30,7 @@ def deleteEvent(userId, eventId):
 def queryEvent(userId):
     cnx = connectDb.getConn()
     with cnx.cursor() as cursor:
-        sql = 'show columns from TB_EVENT'
+        sql = 'select * from TB_EVENT'
         cursor.execute(sql)
         result = cursor.fetchall()
         print (result)
--- temp/initDatabase.sql
+++ temp/initDatabase.sql
@@ -3,7 +3,7 @@ CREATE DATABASE hackforchristmas;
 USE hackforchristmas;
 
 CREATE TABLE TB_EVENT (
-    eventId varchar(30),
+    eventId varchar(50),
     eventName varchar(100),
     eventStarttime varchar(15),
     enventLocation varchar(200),
